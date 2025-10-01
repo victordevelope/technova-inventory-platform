f43260d451d779f2c091577176960d927c6072e9
Victor Martinez <139388619+victordevelope@users.noreply.github.com>
2025-10-01 11:49:50 -0600

    Update product.spec.js

--- tests/e2e/product.spec.js
+++ tests/e2e/product.spec.js
@@ -1,1 +1,9 @@
+describe('Agregar Producto', () => {
+  it('debe crear un producto', () => {
+    cy.visit('/products/new')
+    cy.get('[data-testid="product-name"]').type('Teclado Mecánico')
+    cy.get('[data-testid="save-product"]').click()
+    cy.contains('Producto agregado exitosamente')
+  })
+})
 
